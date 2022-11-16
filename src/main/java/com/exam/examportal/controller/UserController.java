aee62a952338c09f60150445b04679c1c23ab3b1
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2022-11-16 19:01:31 +0530

    created user web service

---
+++ src/main/java/com/exam/examportal/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.exam.examportal.controller;
+
+import com.exam.examportal.entities.Role;
+import com.exam.examportal.entities.User;
+import com.exam.examportal.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @PostMapping("/")
+    public User createUser(@RequestBody User user) throws Exception {
+        Set<User> users = new HashSet<>();
+        users.add(user);
+
+        Role role = new Role();
+        role.setRoleId(2);
+        role.setRoleName("NORMAL");
+        role.setUser(users);
+
+        user.setRole(role);
+        return userService.createUser(user,role);
+    }
+
+    @GetMapping("/{userName}")
+    public User getUser(@PathVariable String userName){
+       return userService.getUser(userName);
+    }
+}
